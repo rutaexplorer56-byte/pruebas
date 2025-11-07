43a4aa31b02ad026c618b94c5a709809cca58d0a
ruta <rutaexplorer56@gmail.com>
2025-11-06 20:58:45 -0500

    subiendo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 
 // Crear transporte SMTP de MailerSend
